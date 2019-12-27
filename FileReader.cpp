564832a8f6412a53371b2e7407152e732032d27e
ik48573 <ivan.kliman@fer.hr>
2019-12-27 15:47:30 +0100

    list filenames starting with J

---
+++ FileReader.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::string path = "C:/Users/Ivan/Desktop/faks/bioinf/fastq";
+    for (const auto& entry : fs::directory_iterator(path)) {
+        std::string fileName = fs::path(entry.path()).filename().string();
+        if (fileName.rfind("J", 0) == 0) {
+            std::cout << fs::path(entry.path()).filename() << std::endl;
+        }
+    }
+}
+
