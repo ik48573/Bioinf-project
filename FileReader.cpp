862e48e105981f6024f610b71ca0ffde5e57f640
ik48573 <ivan.kliman@fer.hr>
2019-12-27 16:48:46 +0100

    Wrote path so it is relative to project. It is important to add fastq files to project now.

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,11 +1,12 @@
 ﻿#include <string>
 #include <iostream>
 #include <filesystem>
+
 namespace fs = std::filesystem;
 
 int main()
 {
-    std::string path = "C:/Users/Ivan/Desktop/faks/bioinf/fastq";
+    std::string path = "fastq";
     for (const auto& entry : fs::directory_iterator(path)) {
         std::string fileName = fs::path(entry.path()).filename().string();
         if (fileName.rfind("J", 0) == 0) {
