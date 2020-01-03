fe36c588e40b73fd46d2962fce1a453c677adeb7
Ana Ćubelić <ana.cubelic@asseco-see.hr>
2020-01-03 15:39:51 +0100

    open file

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,6 +1,8 @@
 ﻿#include <string>
 #include <iostream>
 #include <filesystem>
+#include <sstream>
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -10,7 +12,19 @@ int main()
     for (const auto& entry : fs::directory_iterator(path)) {
         std::string fileName = fs::path(entry.path()).filename().string();
         if (fileName.rfind("J", 0) == 0) {
-            std::cout << fs::path(entry.path()).filename() << std::endl;
+            //std::string pathF = entry.path().string();
+            //std::cout << fs::path(entry.path()).filename() << std::endl;
+            std::cout << pathF << std::endl;
+            std::ifstream fileOpen(entry.path().string());
+            if (!fileOpen.is_open()) {
+                std::perror("File opening failed");
+                return EXIT_FAILURE;
+            }
+            else
+            {
+                std::cout << "Otvoren file\n";
+            }
+            
         }
     }
 }
